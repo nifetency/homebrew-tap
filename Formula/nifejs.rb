60040e724f3e2e4abe0d9204c7ddc02192eb11a4
nida <nida.sahar@gmail.com>
2021-07-26 05:05:44 +0530

    Framework changes for homebrew

--- Formula/nifejs.rb
+++ Formula/nifejs.rb
@@ -1,12 +1,12 @@
-class Flyjs < Formula
-  desc "flyjs cli"
-  homepage "https://fly.io"
+class Nifejs < Formula
+  desc "Nifejs cli"
+  homepage "https://nife.io"
   url "https://get.fly.io/tarballs/stable/flyjs-v0.56.2/flyjs-v0.56.2-darwin-x64.tar.gz"
   sha256 "4deb8d482fdf0b867d6e436c387c31fedb3d70fc56f9c43344a78a44c429a02c"
-  version "0.56.2"
+  version "2.6.0"
 
   def install
-    inreplace "bin/flyjs", /^CLIENT_HOME=/, "export FLY_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
+    inreplace "bin/nifejs", /^CLIENT_HOME=/, "export FLY_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
     libexec.install Dir["*"]
     bin.install_symlink libexec/"bin/flyjs"
   end
