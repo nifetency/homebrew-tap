258d4191e14a4a23959ac98bb8dbbe18c820df1f
nida <nida.sahar@gmail.com>
2021-09-22 02:12:14 +0530

    Update entrypoint.sh

--- .github/actions/update-nife/entrypoint.sh
+++ .github/actions/update-nife/entrypoint.sh
@@ -4,7 +4,7 @@ set -ex
 
 mkdir -p ./tmp
 
-curl "https://get.fly.io/tarballs/stable/darwin-x64" > tmp/manifest.json
+curl "https://docs.nife.io/releases/darwin-x64" > tmp/manifest.json
 URL=$(node -p "require('./tmp/manifest.json').gz")
 SHA=$(node -p "require('./tmp/manifest.json').sha256gz")
 VERSION=$(node -p "require('./tmp/manifest.json').version")
@@ -14,17 +14,17 @@ echo "Updating $VERSION on $CHANNEL"
 echo "  URL: $URL"
 echo "  SHA: $SHA"
 
-sed -i "s~url \".*\"~url \"${URL}\"~" "Formula/flyjs.rb"
-sed -i "s~sha256 \".*\"~sha256 \"${SHA}\"~" "Formula/flyjs.rb"
-sed -i "s~version \".*\"~version \"${VERSION}\"~" "Formula/flyjs.rb"
+sed -i "s~url \".*\"~url \"${URL}\"~" "Formula/nifejs.rb"
+sed -i "s~sha256 \".*\"~sha256 \"${SHA}\"~" "Formula/nifejs.rb"
+sed -i "s~version \".*\"~version \"${VERSION}\"~" "Formula/nifejs.rb"
 
 if [[ -n $(git status -s) ]]; then
-  git add Formula/flyjs.rb
+  git add Formula/nifejs.rb
 
   sh -c "git remote add github 'https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git' \
-        && git config --global user.name 'Flybot' \
-        && git config --global user.email 'bot@fly.io' \
-        && git commit -m 'fly v$VERSION' \
+        && git config --global user.name 'nida' \
+        && git config --global user.email 'nida.sahar@gmail.com' \
+        && git commit -m 'nife v$VERSION' \
         && git push github HEAD:${GITHUB_REF}"
 else 
   echo 'latest version already released, done'
