77005a929c25967260f137396ada18b853f5fc5f
sundar21 <sundar@nife.io>
2022-12-28 21:33:28 +0530

    v0.1.7

--- Formula/nifectl.rb
+++ Formula/nifectl.rb
@@ -7,22 +7,22 @@ class Nifectl < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/nifetency/nife-release/releases/download/prod-0.1.6/nifectl-prod-0.1.6-darwin-amd64.tar.gz"
-      sha256 "46cf7228a83dd8a640c899897ef1c55c0192ddf468a8f1b6e98fa0594620daac"
+      url "https://github.com/nifetency/nife-release/releases/download/prod-v0.1.7/nifectl-prod-v0.1.7-darwin-amd64.tar.gz"
+      sha256 "2dd89a314bea133bfa2a090e0cb747c79c67da9b48f088ec4c5e3cf8fef52ad1"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/nifetency/nife-release/releases/download/prod-0.1.6/nifectl-prod-0.1.6-darwin-amd64.tar.gz"
-      sha256 "46cf7228a83dd8a640c899897ef1c55c0192ddf468a8f1b6e98fa0594620daac"
+      url "https://github.com/nifetency/nife-release/releases/download/prod-v0.1.7/nifectl-prod-v0.1.7-darwin-amd64.tar.gz"
+      sha256 "2dd89a314bea133bfa2a090e0cb747c79c67da9b48f088ec4c5e3cf8fef52ad1"
     end
   end
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/nifetency/nife-release/releases/download/prod-0.1.6/nifectl-prod-0.1.6-linux-386.tar.gz"
-      sha256 "f3b4918cb66520db2b60e70bc792d8f0abba1834e6609d5fa4076386d5aae64a"
+      url "https://github.com/nifetency/nife-release/releases/download/prod-v0.1.7/nifectl-prod-v0.1.7-linux-386.tar.gz"
+      sha256 "acab2be9e34927c1de51146ef66fce41dd3ae0972b92a2b0b280fd5b560fd153"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/nifetency/nife-release/releases/download/prod-0.1.6/nifectl-prod-0.1.6-linux-amd64.tar.gz"
-      sha256 "8e5bfc45bb1fda9da154fc0ad6cf350339b2af8ce20aeefbba9cd460bbf1414d"
+      url "https://github.com/nifetency/nife-release/releases/download/prod-v0.1.7/nifectl-prod-v0.1.7-linux-amd64.tar.gz"
+      sha256 "35ed516780029c21298ee9964144736c1e3d198b67cf5514542da5eb22ddb03f"
     end
   end
 
