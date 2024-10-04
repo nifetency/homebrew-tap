a2a03a2252500d8c69f0443f1f6ce60bc7827e44
Vivek V <88622073+vivekvenkatachala@users.noreply.github.com>
2024-10-04 17:23:58 +0530

    Update nifectl.rb

--- Formula/nifectl.rb
+++ Formula/nifectl.rb
@@ -7,22 +7,22 @@ class Nifectl < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/nifetency/nife-release/releases/download/prod-v-0.3.6/nifectl-prod-v0.3.6-darwin-amd64.tar.gz"
-      sha256 "6F3796B3C66E3A515BABA5F398809DD8A8F0C00C4B14F62674A09398DBF0E99D"
+      url "https://github.com/nifetency/nife-release/releases/download/prod-v0.3.7/nifectl-prod-v0.3.7-darwin-amd64.tar.gz"
+      sha256 "8C40DAB5E9B74594CEC7E57EDE5DD4A5C217119C5259B6228B453A48C30BBB6C"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/nifetency/nife-release/releases/download/prod-v-0.3.6/nifectl-prod-v0.3.6-darwin-amd64.tar.gz"
-      sha256 "6F3796B3C66E3A515BABA5F398809DD8A8F0C00C4B14F62674A09398DBF0E99D"
+      url "https://github.com/nifetency/nife-release/releases/download/prod-v0.3.7/nifectl-prod-v0.3.7-darwin-amd64.tar.gz"
+      sha256 "8C40DAB5E9B74594CEC7E57EDE5DD4A5C217119C5259B6228B453A48C30BBB6C"
     end
   end
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/nifetency/nife-release/releases/download/prod-v-0.3.6/nifectl-prod-v0.3.6-linux-386.tar.gz"
-      sha256 "7379EF6BB14EBF0DCF1EC811C0AFFE0C0AE20178935D661CEB3CA8C36B2F580E"
+      url "https://github.com/nifetency/nife-release/releases/download/prod-v0.3.7/nifectl-prod-v0.3.7-linux-386.tar.gz"
+      sha256 "32F6739359C33285FF85CB46D6FA685F6E442F0FAEDF0A10CE636F6EB341F57F"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/nifetency/nife-release/releases/download/prod-v-0.3.6/nifectl-prod-v0.3.6-linux-amd64.tar.gz"
-      sha256 "1CC17DF1B5FD21244458C842604398E97D1B8281A01FC4F6FFAF98276A8E0AE5"
+      url "https://github.com/nifetency/nife-release/releases/download/prod-v0.3.7/nifectl-prod-v0.3.7-linux-amd64.tar.gz"
+      sha256 "AC54EED9B8391D41C0991649FDBA5BC9E3CF3B7C0030D50E369C6D4737FD4D98"
     end
   end
 
